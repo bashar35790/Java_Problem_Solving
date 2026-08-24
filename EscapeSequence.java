0935a556c845c692a3e3c667060fdb956ae86d92
Abul Bashar <bashar35790@gmail.com>
2026-08-24 20:59:21 +0600

    added escap sequece

---
+++ EscapeSequence.java
@@ -0,0 +1,9 @@
+public class EscapeSequence {
+    public static void main(String[] args){
+        System.out.println("Hello, World!\n Hello, World!");
+        System.out.println("Hello, World!\t Hello, World!");
+        System.out.println("Hello, World!\\ Hello, World!");
+        System.out.println("Hello, World!\" Hello, World!");
+        System.out.println("Hello, World!' Hello, World!");
+    }
+}

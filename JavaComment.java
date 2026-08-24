0bad3edd7e4a59a6a64834a72bf7e108589828b4
Abul Bashar <bashar35790@gmail.com>
2026-08-24 21:01:44 +0600

    added java comment

---
+++ JavaComment.java
@@ -0,0 +1,12 @@
+public class JavaComment {
+    public static void main(String[] args){
+        // This is a single-line comment
+
+        /*
+         * This is a multi-line comment
+         * It can span multiple lines
+         */
+
+        System.out.println("Hello, World!"); // This prints a message to the console
+    }
+}
